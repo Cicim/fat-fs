530397ff6c4e33a4d8fc132173b222857f9c3c3b
Lorenzo Clazzer <lorenzoclazzer.2000@outlook.it>
2022-06-10 17:55:52 +0200

    Improved test aesthetics

--- fat_test.c
+++ fat_test.c
@@ -12,6 +12,8 @@
  * @author Claziero
  */
 void test_bitmap () {
+    printf("******************************TESTING BITMAP FUNCTIONS******************************\n");
+
     FatFs *fs;
     fat_init("fat_test.dat", 32, 64);
     fat_open(&fs, "fat_test.dat");
@@ -19,17 +21,17 @@ void test_bitmap () {
     // Test bitmap_get on any block (e.g. 35) (should return 0)
     int block_number = 35;
     int bitmap_value = bitmap_get(fs, block_number);
-    printf("bitmap_get(%d) = %d (should be 0)\n", block_number, bitmap_value);
+    printf("TEST 1: bitmap_get(%d) \t\t\t= %d (should be 0)\n", block_number, bitmap_value);
 
     // Test bitmap_set on block n.35 (should set the bit to 1)
     bitmap_set(fs, block_number, 1);
 
     // Test bitmap_get on block n.35 (should return 1)
-    printf("bitmap_set(%d, 1) = %d (should be 1)\n", block_number, bitmap_get(fs, block_number));
+    printf("TEST 2: bitmap_set(%d, 1) \t\t= %d (should be 1)\n", block_number, bitmap_get(fs, block_number));
     
     // Test bitmap_get_free_block (should return the first block n.0)
     int free_block = bitmap_get_free_block(fs);
-    printf("bitmap_get_free_block() = %d (should be 0)\n", free_block);
+    printf("TEST 3: bitmap_get_free_block() \t= %d (should be 0)\n", free_block);
 
     // Occupy the first and the third blocks
     bitmap_set(fs, 0, 1);
@@ -37,9 +39,10 @@ void test_bitmap () {
 
     // Try again to get a free block (should return the second block n.1)
     free_block = bitmap_get_free_block(fs);
-    printf("bitmap_get_free_block() = %d (should be 1)\n", free_block);
+    printf("TEST 4: bitmap_get_free_block() \t= %d (should be 1)\n", free_block);
 
     fat_close(fs);
+    printf("************************************************************************************\n\n");
 }
 
 /**
@@ -47,6 +50,8 @@ void test_bitmap () {
  * @author Claziero
  */
 void test_path_absolute () {
+    printf("***************************TESTING PATH_ABSOLUTE FUNCTION***************************\n");
+
     FatFs *fs;
     fat_init("fat_test.dat", 16, 64);
     fat_open(&fs, "fat_test.dat");
@@ -125,6 +130,7 @@ void test_path_absolute () {
     printf("\tpath_get_absolute(\"..a\") = %s\n", fs->current_directory);
 
     fat_close(fs);
+    printf("************************************************************************************\n\n");
 }
 
 int main(int argc, char **argv) {
