231f8b30c922a4e948f56cce05e0f675ebe353e1
Lorenzo Clazzer <lorenzoclazzer.2000@outlook.it>
2022-06-13 21:12:28 +0200

    Changed file_read function, Now it works with more blocks

--- libfat/file_read.c
+++ libfat/file_read.c
@@ -3,7 +3,7 @@
  * @author Claziero
  */
 
-#include "fat.h"
+#include "internals.h"
 #include <stdlib.h>
 #include <string.h>
 
@@ -21,49 +21,50 @@ int file_read(FileHandle *file, char *buffer, int size) {
     int curr_block = file->initial_block_number;
     int total_offset = 0;
     char *data;
-    while (curr_block != file->current_block_number)
-        total_offset += file->fs->header->block_size;
+
+    if (file->current_block_number != file->initial_block_number) {
+        do {
+            total_offset += file->fs->header->block_size;
+            curr_block = fat_get_next_block(file->fs, curr_block);
+        }    
+        while (fat_get_next_block(file->fs, curr_block) != FAT_EOF);
+    }        
     total_offset += file->offset - sizeof(FileHeader);
 
     // Check if size exceeds the file size from the current offset
     if (total_offset + size >= file->fh->size)
         size = file->fh->size - total_offset;
 
-    // Check if offset + size exceeds the block size
+    // Read the data
     int read_size = 0;
-    while (file->offset + size >= file->fs->header->block_size) {
+    while (size > 0) {
         // Go to the current block of the file
         data = file->fs->blocks_ptr 
             + file->current_block_number * file->fs->header->block_size 
             + file->offset;
 
         // Read until the end of the block
-        memcpy(buffer, data, file->fs->header->block_size - file->offset);
+        int size_to_read = size > file->fs->header->block_size - file->offset ?
+            file->fs->header->block_size - file->offset : size;
+        memcpy(buffer, data, size_to_read);
 
         // Update the size of the read and the buffer pointer
-        read_size += file->fs->header->block_size - file->offset;
+        read_size += size_to_read;
         size -= read_size;
         buffer += read_size;
 
-        // Update the offset and the current block number
-        file->offset = 0;
-        file->current_block_number = file->fs->fat_ptr[file->current_block_number];
-
         // Check if the current block is the end of the file
-        if (file->current_block_number == -1)
-            return read_size;
+        if (fat_get_next_block(file->fs, file->current_block_number) != FAT_EOF) {
+            // Update the offset and the current block number
+            file->current_block_number = fat_get_next_block(file->fs, file->current_block_number);
+            file->offset = 0;
+        }
+        else {
+            // Else update the offset
+            file->offset += size_to_read;
+            return read_size;  
+        } 
     }
-    if (read_size != 0)
-        return read_size;
-        
-    // Else read for the given size
-    data = file->fs->blocks_ptr
-        + file->current_block_number * file->fs->header->block_size 
-        + file->offset;
-    memcpy(buffer, data, size);
 
-    // Update the offset
-    file->offset += size;
-
-    return size;
+    return read_size;
 }
